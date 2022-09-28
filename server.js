77db89e5204a48d0d5960ca8b06e5ce80ac9c001
Arashi233 <97686561+Arashi233@users.noreply.github.com>
2022-09-28 17:16:28 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -6,20 +6,20 @@ const path = require('path');
 const fs = require('fs');
 const userList = new Array();
 const router = require('./router')
-var g_onlines = {} // 所有在线玩家
-var g_commands = new Array() // 指令数组
-var g_commands_histroy = new Array() // 历史指令，用于断线重连
-var g_joinCount = 0 // 已准备的人数
-var g_maxJoinCount = 2 // 最大人数
-var g_stepTime = 0 // 当前step时间戳
-var g_stepInterval = 100 // 每个step的间隔ms
+let g_onlines = {} // 所有在线玩家
+let g_commands = new Array() // 指令数组
+let g_commands_histroy = new Array() // 历史指令，用于断线重连
+let g_joinCount = 0 // 已准备的人数
+let g_maxJoinCount = 2 // 最大人数
+let g_stepTime = 0 // 当前step时间戳
+let g_stepInterval = 100 // 每个step的间隔ms
 
 // 游戏状态枚举
-var STATUS = {
+const STATUS = {
 	WAIT:1,
 	START:2
 }
-var g_gameStatus = STATUS.WAIT;
+let g_gameStatus = STATUS.WAIT;
 let timer = 60;
 //开发静态资源
 app.engine('html',require('express-art-template'))
@@ -30,7 +30,7 @@ io.on('connection', function(socket){
 
 	// 获取用户账户
 	function getAccount(socketId) {
-		for(var key in g_onlines) {
+		for(let key in g_onlines) {
 			if(socketId == g_onlines[key].socket.id) {
 				return key
 			}
@@ -62,12 +62,12 @@ io.on('connection', function(socket){
 		socket.emit('timeSync', {client:time, server:Date.now()})
 	})
 	socket.on('disconnect', function () {
-		var account = getAccount(socket.id)
+		let account = getAccount(socket.id)
 		if(account) {
 			g_onlines[account].online = false
 			console.log(account, "退室した")
-			var isGameOver = true
-			for(var key in g_onlines) {
+			let isGameOver = true
+			for(let key in g_onlines) {
 				if(g_onlines[key].online) {
 					isGameOver = false
 				}
@@ -102,24 +102,24 @@ io.on('connection', function(socket){
 // step定时器
 function stepUpdate() {
 	// 过滤同帧多次指令
-	var message = {}
-	for(var key in g_onlines) {
+	let message = {}
+	for(let key in g_onlines) {
 		message[key] = {step:g_stepTime, id:key}
 	}
-	for(var i = 0; i < g_commands.length; ++i) {
-		var command = g_commands[i]
+	for(let i = 0; i < g_commands.length; ++i) {
+		let command = g_commands[i]
 		command.step = g_stepTime
 		message[command.id] = command
 	}
 	g_commands = new Array()
 
 	// 发送指令
-	var commands = new Array()
-	for(var key in message) {
+	let commands = new Array()
+	for(let key in message) {
 		commands.push(message[key])
 	}
 	g_commands_histroy.push(commands)
-	for(var key in g_onlines) {
+	for(let key in g_onlines) {
 		g_onlines[key].socket.emit('message', new Array(commands))
 	}
 }
